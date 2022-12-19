8ef7786bbbb79bbd81ac6b4c6573f05d18cc417f
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2022-12-19 12:03:10 +0100

    chore: Don't reverse top sums array

--- 01.py
+++ 01.py
@@ -11,11 +11,11 @@ top_3_sums = [0, 0, 0]
 
 for calory in calories:
     if calory == "":
-        if current_sum > top_3_sums[0]:
-            for i in reversed(range(3)):
+        if current_sum > top_3_sums[2]:
+            for i in range(3):
                 if current_sum > top_3_sums[i]:
-                    top_3_sums.insert(i + 1, current_sum)
-                    top_3_sums.pop(0)
+                    top_3_sums.insert(i, current_sum)
+                    top_3_sums.pop(3)
                     break
 
         current_sum = 0
@@ -23,5 +23,5 @@ for calory in calories:
         current_sum = current_sum + int(calory)
 
 
-print("Top value:", top_3_sums[2])
-print("Top 3 sum:", reduce(lambda a, b: a + b, top_3_sums))
+print("Top value:", top_3_sums[0]) # 70369
+print("Top 3 sum:", reduce(lambda a, b: a + b, top_3_sums)) # 203002
