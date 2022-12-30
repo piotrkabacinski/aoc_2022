47c5eb9b3292be8b22a9158c808a792233b65fc8
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2022-12-30 13:36:56 +0100

    chore: Optimize message look up

--- 06.py
+++ 06.py
@@ -7,19 +7,27 @@ from get_input import get_input
 
 stream = get_input("https://adventofcode.com/2022/day/6/input")[0]
 
-characters = enumerate([*stream])
-
 
 class CharacterAmount(Enum):
     START_OF_PACKET = 4
     START_OF_MESSAGE = 14
 
 
-amount = CharacterAmount.START_OF_PACKET.value
+packet_start_index = 0
 
-for index, _ in characters:
-    sequence = stream[index:index + amount]
+for index, _ in enumerate([*stream]):
+    sequence = stream[index:index + CharacterAmount.START_OF_PACKET.value]
 
-    if (len(set(sequence)) == amount):
-        print(index + amount)  # 1766, 2383
+    if (len(set(sequence)) == CharacterAmount.START_OF_PACKET.value):
+        packet_start_index = index
+
+        print(packet_start_index + CharacterAmount.START_OF_PACKET.value)  # 1766
+        break
+
+
+for index in range(packet_start_index + CharacterAmount.START_OF_PACKET.value, len([*stream])):
+    sequence = stream[index:index + CharacterAmount.START_OF_MESSAGE.value]
+
+    if (len(set(sequence)) == CharacterAmount.START_OF_MESSAGE.value):
+        print(index + CharacterAmount.START_OF_MESSAGE.value)  # 2383
         break
